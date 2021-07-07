b77e39b5a42a41a68aa2af25421a239fb3310081
Wendy-Espinales14 <56566932+Wendy-Espinales14@users.noreply.github.com>
2021-07-07 08:43:34 -0500

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -1,8 +1,14 @@
 /*consulta1*/
+select  
+tios_as.id_tios as id_tios,
+(tios_as.nombres ||' '||tios_as.apellidos) as nombre_tios,
+tios_as.n_año as año,
+count (niños_del_orfanato) as numero_de_niños
+from  niños_del_orfanato
+inner join tios_as on tios_as.id_tios = niños_del_orfanato.id_niño
+GROUP BY tios_as.nombres , niños_del_orfanato.id_niño,tios_as.id_tios, tios_as.n_año, tios_as.apellidos
+order by id_tios
 
-select tios_as.nombres, tios_as.apellidos ,tios_as.n_año, 
-tios_as.cantida_huerfanos from tios_as
-order by tios_as.n_año
 /*consulta 2*/
 select 
 instituciones_educativas.nombre as institucion,
@@ -12,12 +18,12 @@ inner join instituciones_educativas on instituciones_educativas.id_institucion =
 where historial_años_aprobados.estado_academico='graduado'
 GROUP BY instituciones_educativas.nombre;
 /*consulta 3*/
-select 
+select  
 tipos_orfandad.nombre as id_tipo,
 count (tipos_orfandad.nombre) as tipo_orfandad
-from tipos_orfandad
-GROUP BY tipos_orfandad.nombre;
-/*consulta 4*/
+from tipo_orfandad
+GROUP BY tipos_orfandad.nombre ;
+/* consulta 4*/
 select 
 actividades.nombre_actividad as nombre,
 actividades.year_año_actividad as año_actividad,
